ad1d03352805aa17726cc99ab1579df4de76002c
Aldo-A <aldoalmeidaa@yahoo.com>
2020-08-07 23:01:15 -0400

    Progress

--- src/test/java/com/stayready/poll_application/dtosTest/OptionCountTest.java
+++ src/test/java/com/stayready/poll_application/dtosTest/OptionCountTest.java
@@ -1,4 +1,64 @@
 package com.stayready.poll_application.dtosTest;
 
+import com.stayready.poll_application.dtos.OptionCount;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class OptionCountTest {
+
+    @Test
+    public void getOptionId(){
+        // Given
+        Long id=453134L;
+
+        // When
+        OptionCount option=new OptionCount();
+        option.setOptionId(id);
+        Long actual= option.getOptionId();
+
+        // Then
+        Assert.assertEquals(id,actual);
+    }
+
+    @Test
+    public void setOptionIdTest(){
+        // Given
+        Long id=9032034L;
+
+        // When
+        OptionCount option=new OptionCount();
+        option.setOptionId(id);
+        Long actual= option.getOptionId();
+
+        // Then
+        Assert.assertEquals(id,actual);
+    }
+
+    @Test
+    public void getCountTest(){
+        // Given
+        int count=432;
+
+        // When
+        OptionCount option=new OptionCount();
+        option.setCount(count);
+        int actual= option.getCount();
+
+        // Then
+        Assert.assertEquals(count,actual);
+    }
+
+    @Test
+    public void setCountTest(){
+        // Given
+        int count=0;
+
+        // When
+        OptionCount option=new OptionCount();
+        option.setCount(count);
+        int actual= option.getCount();
+
+        // Then
+        Assert.assertEquals(count,actual);
+    }
 }
--- src/test/java/com/stayready/poll_application/dtosTest/VoteResultTest.java
+++ src/test/java/com/stayready/poll_application/dtosTest/VoteResultTest.java
@@ -1,4 +1,71 @@
 package com.stayready.poll_application.dtosTest;
 
+import com.stayready.poll_application.dtos.OptionCount;
+import com.stayready.poll_application.dtos.VoteResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class VoteResultTest {
+
+    @Test
+    public void setCountTest(){
+        // Given
+        int count=4243432;
+
+        // When
+        VoteResult results=new VoteResult();
+        results.setTotalVotes(count);
+        int actual= results.getTotalVotes();
+
+        // Then
+        Assert.assertEquals(count,actual);
+    }
+
+    @Test
+    public void getCountTest(){
+        // Given
+        int count=23;
+
+        // When
+        VoteResult results=new VoteResult();
+        results.setTotalVotes(count);
+        int actual= results.getTotalVotes();
+
+        // Then
+        Assert.assertEquals(count,actual);
+    }
+
+    @Test
+    public void setOptionCountTest(){
+        // Given
+        Collection<OptionCount> options=new ArrayList<OptionCount>();
+        options.add(new OptionCount());
+        options.add(new OptionCount());
+        options.add(new OptionCount());
+        options.add(new OptionCount());
+
+        // When
+        VoteResult results=new VoteResult();
+        results.setResults(options);
+
+        // Then
+        Assert.assertEquals(options,results.getResults());
+    }
+
+    @Test
+    public void getOptionCountTest(){
+        // Given
+        Collection<OptionCount> options=new ArrayList<OptionCount>();
+        options.add(new OptionCount());
+
+        // When
+        VoteResult results=new VoteResult();
+        results.setResults(options);
+
+        // Then
+        Assert.assertEquals(options,results.getResults());
+    }
 }
